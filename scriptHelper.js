b6f20fc2cc3485eed6863c4b308df619b0b6e83a
Travis <azai392@gmail.com>
2024-03-11 23:13:11 -0500

    Task 2 finished and first 2 tests passing

--- scriptHelper.js
+++ scriptHelper.js
@@ -33,20 +33,45 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
         let faultyItems = document.getElementId("faultyItems");
         let fuelStatus = document.getElementId("fuelStatus");
         let launchStatus = document.getElementId("launchStatus");
-        let cargoMass = document.getElementId("cargoStatus");
+        let cargoStatus = document.getElementId("cargoStatus");
 
-        // Add Pilot and Copilot names 
-        pilotStatus.innerHTML = `Pilot: ${pilot.value}`;
-        copilotStatus.innerHTML = `Co-pilot: ${copilot.value}`;
+
+        // validate that all fields are filled and pushing an alert if not.
+        if (validateInput(pilot) === 'Empty' || validateInput(copilot) === 'Empty' || validateInput(fuelLevel) === 'Empty' || validateInput(cargoLevel) === 'Empty' ) {
+            alert ('All fields required. Please enter all required information.')
+        }
+        // checks to make sure pilot and copilot are strings and fuel and cargo are numbers
+        else if (validateInput(pilot) === 'Is a Number' || validateInput(copilot) === 'Is a Number') {
+            alert ('Number entered in Pilot and Co-pilot fields. Please enter a name.')
+        }  
+        else if (validateInput(fuelLevel) === 'Not a Number' || validateInput(cargoLevel) === 'Not a Number') {
+            alert ('Invalid entry. Please enter a numeric value for Fuel and Cargo.')
+        } 
+        // Add Pilot and Copilot names if all fields are correctly filled
+        else {
+            pilotStatus.innerHTML = `Pilot: ${pilot.value}`;
+            copilotStatus.innerHTML = `Co-pilot: ${copilot.value}`;
+        }
 
         // Change faultyItems to visible, launch Status to red text, and to display "Shuttle not ready for launch" if fuel levels is too low (< 10000)
-        faultyItems.style.visibility = 'visible';
-        launchStatus.innerHTML = "Shuttle not ready for launch";
-        launchStatus.style.color = 'red';
-
+        if (Number(fuelLevel) < 10000) {
+            faultyItems.style.visibility = 'visible';
+            fuelStatus.innerHTML = 'Not enough fuel for journey.'
+            launchStatus.innerHTML = "Shuttle not ready for launch";
+            launchStatus.style.color = 'red';
+        } 
         // if cargo mass > 10000 change list to visible with an updated cargo status that states there is too much mass for shuttle to launch and change launch Status to red
-
-
+        else if (Number(cargoLevel) > 10000) {
+            list.visibility = 'visible';
+            cargoStatus.innerHTML = 'Too Much Mass in Cargo Area';
+            launchStatus.innerHTML = "Shuttle not ready for launch";
+            launchStatus.style.color = 'red';
+        }
+        // if shuttle is ready to launch then change launch Status to green and display "Shuttle is ready for launch"
+        else if (Number(fuelLevel) > 10000 || Number(cargoLevel) < 10000) {
+            launchStatus.innerHTML = 'Shuttle is ready for launch';
+            launchStatus.style.color = 'green';
+        }   
  }
  
  async function myFetch() {
