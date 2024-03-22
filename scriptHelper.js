e5180cac65d45b83394d9d4d01d72907025b889c
Travis <azai392@gmail.com>
2024-03-21 19:57:25 -0500

    fixed display if/else statements to display the rigth thing

--- scriptHelper.js
+++ scriptHelper.js
@@ -38,14 +38,14 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 
         // validate that all fields are filled and pushing an alert if not.
         if (validateInput(pilot) === 'Empty' || validateInput(copilot) === 'Empty' || validateInput(fuelLevel) === 'Empty' || validateInput(cargoLevel) === 'Empty' ) {
-            alert('All fields required. Please enter all required information.');
+            return alert('All fields required. Please enter all required information.');
         }
         // checks to make sure pilot and copilot are strings and fuel and cargo are numbers
         else if (validateInput(pilot) === 'Is a Number' || validateInput(copilot) === 'Is a Number') {
-            alert('Number entered in Pilot and Co-pilot fields. Please enter a name.');
+           return alert('Number entered in Pilot and Co-pilot fields. Please enter a name.');
         }  
         else if (validateInput(fuelLevel) === 'Not a Number' || validateInput(cargoLevel) === 'Not a Number') {
-            alert('Invalid entry. Please enter a numeric value for Fuel and Cargo.');
+           return  alert('Invalid entry. Please enter a numeric value for Fuel and Cargo.');
         } 
         // Add Pilot and Copilot names if all fields are correctly filled
         else {
@@ -53,25 +53,40 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
             copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
         }
 
-        // Change faultyItems to visible, launch Status to red text, and to display "Shuttle not ready for launch" if fuel levels is too low (< 10000)
-        if (Number(fuelLevel) < 10000) {
+
+        //update to compare both fuelLevel and CargoMass on each check
+
+        // Change faultyItems to visible, launch Status to red text, and to display "Shuttle not ready for launch" if fuel levels is too low (< 10000) 
+        if (Number(fuelLevel) < 10000 && Number(cargoLevel) < 10000) {
             faultyItems.style.visibility = 'visible';
-            fuelStatus.innerHTML = 'Fuel level too low for launch'
+            fuelStatus.innerHTML = 'Fuel level too low for launch';
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+            cargoStatus.innerHTML = 'Cargo mass low enough for launch';
             launchStatus.style.color = 'red';
         } 
         // if cargo mass > 10000 change list to visible with an updated cargo status that states there is too much mass for shuttle to launch and change launch Status to red
-        else if (Number(cargoLevel) > 10000) {
-            list.style.visibility = 'visible';
-            fuelStatus.innerHTML = 'Fuel level high enough for launch'
+        else if (Number(cargoLevel) > 10000 && Number(fuelLevel) > 10000) {
+            faultyItems.style.visibility = 'visible';
+            fuelStatus.innerHTML = 'Fuel level high enough for launch';
             cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
             launchStatus.style.color = 'red';
         }
+
+        // if both cargo and fuel are not at the correct levels to launch
+        else if (Number(cargoLevel) > 10000  && Number(fuelLevel) < 10000) {
+            faultyItems.style.visibility = 'visible';
+            fuelStatus.innerHTML = 'Fuel level too low for launch';
+            cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
+            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+            launchStatus.style.color = 'red';
+
+        }
+         
         // if shuttle is ready to launch then change launch Status to green and display "Shuttle is ready for launch"
         else if (Number(fuelLevel) > 10000 || Number(cargoLevel) < 10000) {
             launchStatus.innerHTML = 'Shuttle is Ready for Launch';
-            fuelStatus.innerHTML = 'Fuel level high enough for launch'
+            fuelStatus.innerHTML = 'Fuel level high enough for launch';
             cargoStatus.innerHTML = 'Cargo mass low enough for launch';
             launchStatus.style.color = 'green';
         }   
